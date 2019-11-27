e89447e3fe4350ef97d2b053608f2fe3ec208ec4
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-11-27 17:48:34 -0300

    :construction: Base Templace Component

---
+++ src/components/templates/BaseTemplate/BaseTemplate.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <PageHeader />
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import PageHeader from '@/components/organisms/PageHeader'
+
+export default {
+  name: 'base-template',
+  components: {
+    PageHeader
+  }
+}
+</script>
