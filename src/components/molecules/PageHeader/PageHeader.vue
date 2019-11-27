0ef4d408ad0b4e51f4993d71ad178776a728f647
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-11-27 14:22:49 -0300

    :construction: Create Page Header component

---
+++ src/components/molecules/PageHeader/PageHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <VolantyLogo />
+  </div>
+</template>
+
+<script>
+import VolantyLogo from '@/components/atoms/VolantyLogo'
+export default {
+  name: 'page-header',
+  components: {
+    VolantyLogo
+  }
+}
+</script>
