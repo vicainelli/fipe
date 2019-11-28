2e285b1a1da2537d784ba87bc5f8fe7bb7aafda4
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-11-28 19:04:14 -0300

    :wrench: Update for vuejs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "@vue/prettier",
+    "plugin:prettier/recommended"
+  ],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
