89019bf089e7d196e93ee985f4596f108825392d
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-11-28 14:52:39 -0300

    :construction: Adding Form component

--- src/components/pages/Home/Home.vue
+++ src/components/pages/Home/Home.vue
@@ -1,14 +1,18 @@
 <template>
-  <BaseTemplate title="Quanto vale meu carro?"></BaseTemplate>
+  <BaseTemplate title="Quanto vale meu carro?">
+    <FormCalculateVehiclePrice />
+  </BaseTemplate>
 </template>
 
 <script>
 import BaseTemplate from '@/components/templates/BaseTemplate'
+import FormCalculateVehiclePrice from '@/components/organisms/FormCalculateVehiclePrice'
 
 export default {
   name: 'page-home',
   components: {
-    BaseTemplate
+    BaseTemplate,
+    FormCalculateVehiclePrice
   }
 }
 </script>
