29bd834a6f0a4312fde84af5d39146c270b7f51e
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-11-28 07:22:18 -0300

    :construction: Page Home component

---
+++ src/components/pages/Home/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <BaseTemplate>
+
+  </BaseTemplate>
+</template>
+
+<script>
+import BaseTemplate from '@/components/templates/BaseTemplate'
+
+export default {
+  name: 'page-home',
+  components: {
+    BaseTemplate
+  }
+}
+</script>
