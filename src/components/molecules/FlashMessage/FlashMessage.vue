3ed9a17931ddca88eadf7d71e4dc2398077d4ba5
Vinicius Cainelli <vicainelli+github@gmail.com>
2019-12-02 12:12:07 -0300

    :construction: Component for flash messages

---
+++ src/components/molecules/FlashMessage/FlashMessage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <slot>{{ content.msg }}</slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'flash-message',
+  props: {
+    content: {
+      type: Object,
+      default: undefined
+    }
+  }
+}
+</script>
